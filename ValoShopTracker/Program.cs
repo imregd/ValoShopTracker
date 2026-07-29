6753c802cf42ae26e8e08efd8d3edac77d9b0675
imregd <avasjakins87@gmail.com>
2026-07-29 03:22:49 +0100

    added first command (Does nothing as no module to actually handle command, just pings user with command ran), added a service provider for future modules to use (pass in db context etc), removed all ! commands as not needed

--- ValoShopTracker/Program.cs
+++ ValoShopTracker/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Immutable;
+using System.Reflection;
 using Discord;
+using Discord.Interactions;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 using ValoShopTracker;
+using Discord.Net;
+using Microsoft.Extensions.DependencyInjection;
+
 
 public class Program
 {
 
     private static DiscordSocketClient _client;
-
+	
+	
 	
     private static Task Log(LogMessage msg)
     {
@@ -19,15 +25,51 @@ public class Program
 	
     public static async Task Main()
     {
-        _client = new DiscordSocketClient();
-		
+	    var config = JsonConvert.DeserializeObject<DiscToken>(File.ReadAllText("appsettings.json"));
+        
+	    
+	    _client = new DiscordSocketClient();
+        var interactionService = new InteractionService(_client.Rest);
+
+        var collection = new ServiceCollection()
+	        .AddSingleton(_client)
+	        .AddSingleton(interactionService);
+
+        var serviceProvider = collection.BuildServiceProvider();
+
+        _client.Ready += async () =>
+        {
+	        Commands.ClientOnReady(_client);
+        };
+
+        _client.InteractionCreated += async (x) =>
+        {
+	        var ctx = new SocketInteractionContext(_client, x);
+	        await interactionService.ExecuteCommandAsync(ctx, serviceProvider);
+
+	        if (x is SocketSlashCommand cmd)
+	        {
+		        var name = cmd.Data.Name;
+		        await x.RespondAsync($"{ctx.User.Mention}, you called {name}");
+	        }
+        };
+        
         _client.Log += Log;
+        
+        
+        _client.Ready += async () =>
+        {
+	        await interactionService.RegisterCommandsToGuildAsync(config.GuildId);
+        };
 		
-        var token = JsonConvert.DeserializeObject<DiscToken>(File.ReadAllText("appsettings.json")).Token;
-		
-        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.LoginAsync(TokenType.Bot, config.Token);
         await _client.StartAsync();
 		
+        var mod = await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), serviceProvider);
+		
+        Console.WriteLine("mods found", mod.Count());
+
+
 		
         await Task.Delay(-1);
 		
